196f4dd933a474aba5bccab00642ea0d516f53fd
Kunhee Kim <kunheekim@korea.ac.kr>
2022-01-03 16:01:44 +0900

    utterance comments

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -17,7 +17,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: '', // Your repository for archive comment
+    utterances: 'kunheekimdev/kunheekimdev.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
